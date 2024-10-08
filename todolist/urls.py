f547b62d5a282e0a1960dbe55e8aed40c39976fc
Bernardo Mota <bmota703@gmail.com>
2024-10-08 18:07:13 -0300

    Created function to add and update tasks
    I'm going to change the delete function, so it is not working right now

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todolist.views import index, completed, pending, changeCompleteStatus, delete
+from todolist.views import index, completed, pending, changeCompleteStatus, delete, createTask, editTask
 
 app_name = 'todo'
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', index , name='index'),
     path('completed/', completed , name='completed'),
     path('pending/', pending , name='pending'),
-    path('change/<int:id>', changeCompleteStatus , name='changeCompleteStatus'),
-    path('delete/<int:id>', delete , name='delete'),
+    path('change/<int:id>/', changeCompleteStatus , name='changeCompleteStatus'),
+    path('delete/<int:id>/', delete , name='delete'),
+    path('create-task/', createTask , name='create'),
+    path('edit-task/<int:id>/', editTask , name='edit'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from todolist.models import Task
+from datetime import datetime
+from django.urls import reverse
 
 def index(request):
-    tasks = Task.objects.all().order_by('-id')
+    tasks = Task.objects.all().order_by('-id', )
     context = tasks
     return render(request=request, 
                   template_name='todolist/pages/index.html',
@@ -10,6 +12,7 @@ def index(request):
                       'tasks':context,
                       }
                   )
+
     
 def completed(request):
     tasks = Task.objects.filter(completed=True).order_by('-id')
@@ -21,6 +24,7 @@ def completed(request):
                       'filter': 'completed'
                       }
                   )
+
     
 def pending(request):    
     tasks = Task.objects.filter(completed=False).order_by('-id')
@@ -33,23 +37,69 @@ def pending(request):
                       }
                   )
     
-def changeCompleteStatus(request, id):
 
-    task = Task.objects.filter(id=id).first()
-    if task.completed:
-        task.completed = False
-    else:
-        task.completed = True
-        print('Entrou auqi')
+def createTask(request):
+    form_action = reverse('todo:create')
+    
+    if request.method == 'POST':
+        task_title = request.POST.get('task_title')
         
-    task.save()
+        if task_title:
+            new_task = Task(title=task_title)
+            new_task.save()
+        
+            return redirect('todo:index')
+    
+    context = {
+        'action': 'Create',
+        'form-action': form_action
+    }
+    return render(request=request, template_name='todolist/pages/new_task.html', context=context)
+
+
+def editTask(request, id):
+    task = get_object_or_404(Task, id=id)
+    
+    form_action = reverse('todo:edit', args=(id,))
+    task_title = task.title
+    
+    context = {
+        'title': task_title,
+        'action': 'Edit',
+        'form_action': form_action,
+    }
+    
+    if request.method == 'POST':
+        new_task_title = request.POST.get('task_title')
+        task.title = new_task_title
+        task.save()
+        
+        return redirect('todo:index')
+    
+    return render(request=request, template_name='todolist/pages/new_task.html', context=context)
+
+
+def delete(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.delete()
     
     previous_url = request.META.get('HTTP_REFERER', 'todo:index')
     return redirect(previous_url)
 
-def delete(request, id):
-    task = Task.objects.filter(id=id).first()
-    task.delete()
+
+def changeCompleteStatus(request, id):
+
+
+    task = get_object_or_404(Task, id=id)
+    if task.completed:
+        task.completed = False
+        task.completed_at = None
+    else:
+        task.completed = True
+        current_time = datetime.now()
+        task.completed_at = current_time
+        
+    task.save()
     
     previous_url = request.META.get('HTTP_REFERER', 'todo:index')
     return redirect(previous_url)
